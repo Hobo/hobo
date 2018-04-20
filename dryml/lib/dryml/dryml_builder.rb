70d690cc6fb818c3476c5e89cdf607091c3d128a
w0www <imanol@alvarezperez.net>
2018-04-20 14:32:17 +0200

    fix error cant modify String

--- dryml/lib/dryml/dryml_builder.rb
+++ dryml/lib/dryml/dryml_builder.rb
@@ -6,7 +6,7 @@ module Dryml
 
     def initialize(template)
       @template = template
-     @build_instructions = nil # set to [] on the first add_build_instruction
+      @build_instructions = nil # set to [] on the first add_build_instruction
       @part_names = []
     end
 
@@ -59,56 +59,89 @@ module Dryml
            "; output_buffer; end; end")
     end
 
-    begin
-      require "erubis"
-    rescue LoadError
-      # nothing
-    else
-      erubis = ::Erubi::Engine.new(::Erubis::Eruby) do
-      # :nodoc: all    class Erubi < ::Erubi::Engine
-        def add_preamble(src)
 
-        end
+      begin
+        require "erubis"
+      rescue LoadError
+        # nothing
+      else
+        erubis = Class.new(::Erubis::Eruby) do
+          # :nodoc: all
+          def add_preamble(src)
+            @newline_pending = 0
+            src << "@output_buffer = output_buffer || ActionView::OutputBuffer.new;"
+          end
 
-        def add_text(src, text)
-          return if text.empty?
-          src << "self.output_buffer.safe_concat('" << escape_text(text) << "');"
-        end
+          def add_text(src, text)
+            return if text.empty?
 
-        BLOCK_EXPR = /\s+(do|\{)(\s*\|[^|]*\|)?\s*\Z/
+            if text == "\n"
+              @newline_pending += 1
+            else
+              src << "@output_buffer.safe_append='"
+              src << "\n" * @newline_pending if @newline_pending > 0
+              src << escape_text(text)
+              src << "'.freeze;"
 
-        def add_expr_literal(src, code)
-          if code =~ BLOCK_EXPR
-            src << 'self.output_buffer.append= ' << code << ";\nself.output_buffer;"
-          else
-            src << 'self.output_buffer.append= (' << code << ");\nself.output_buffer;"
+              @newline_pending = 0
+            end
+          end
+
+          # Erubis toggles <%= and <%== behavior when escaping is enabled.
+          # We override to always treat <%== as escaped.
+          def add_expr(src, code, indicator)
+            case indicator
+            when "=="
+              add_expr_escaped(src, code)
+            else
+              super
+            end
+          end
+
+          BLOCK_EXPR = /\s*((\s+|\))do|\{)(\s*\|[^|]*\|)?\s*\Z/
+
+          def add_expr_literal(src, code)
+            flush_newline_if_pending(src)
+            if BLOCK_EXPR.match?(code)
+              src << "@output_buffer.append= " << code
+            else
+              src << "@output_buffer.append=(" << code << ");"
+            end
+          end
+
+          def add_expr_escaped(src, code)
+            flush_newline_if_pending(src)
+            if BLOCK_EXPR.match?(code)
+              src << "@output_buffer.safe_expr_append= " << code
+            else
+              src << "@output_buffer.safe_expr_append=(" << code << ");"
+            end
+          end
+
+          def add_stmt(src, code)
+            flush_newline_if_pending(src)
+            super
+          end
+
+          def add_postamble(src)
+            flush_newline_if_pending(src)
+            src << "@output_buffer.to_s"
+          end
+
+          def flush_newline_if_pending(src)
+            if @newline_pending > 0
+              src << "@output_buffer.safe_append='#{"\n" * @newline_pending}'.freeze;"
+              @newline_pending = 0
+            end
           end
         end
-
-        def add_stmt(src, code)
-          # skip fallback code - it utterly destroys DRYML-generated ERB
-          super
-        end
-
-        def add_expr_escaped(src, code)
-          if code =~ BLOCK_EXPR
-            src << "self.output_buffer.safe_append= " << code << ";\nself.output_buffer;"
-          else
-            src << "self.output_buffer.safe_concat((" << code << ").to_s);"
-          end
-        end
-
-        def add_postamble(src)
-          # NOTE: we can't just add a 'self.output_buffer' here because this parser
-          # is used to compile taglibs which don't HAVE one
-        end
-      end
     end
 
     def erb_process(erb_src)
       trim_mode = ActionView::Template::Handlers::ERB.erb_trim_mode
       erb = ::Erubi::Engine.new(erb_src, :trim_mode => trim_mode)
       res = erb.src
+      res = res.dup
       if res.respond_to? :force_encoding
         res.force_encoding(erb_src.encoding)
       end
